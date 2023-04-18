868ab53355300ac542052edb589e52c90375f7d7
Ramil <rdgalimov@yandex.ru>
2023-04-18 09:24:30 +0300

    Домашнее задание

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,6 +5,7 @@ import service.EmployeeDaoImpl;
 public class Application {
     public static void main(String[] args) {
         EmployeeDAO employeeDAO = new EmployeeDaoImpl();
+
         employeeDAO.getById(10);
 
         Employee employee1 = new Employee("Polina", "Ivanova", "Female", 35, 1);
@@ -16,61 +17,5 @@ public class Application {
 
         employeeDAO.deleteEmployee(employee1);//проблема здесь
         employeeDAO.getAllEmployee();
-
-
-
-
-
-
-
-
-
-//        final String user = "postgres";
-//        final String password = "123654";
-//        final String url = "jdbc:postgresql://localhost:5432/skypro";
-//
-//        City city1 = new City(3);
-//        Employee employee1 = new Employee(9, "Petr", "Gerasimov", "Male", 55, city1);
-//        EmployeeDAO employeeDAO = new EmployeeDaoImpl();
-//        //Добавить сотрудника:
-//        employeeDAO.add(employee1);
-//
-//        try (final Connection connection =
-//                     DriverManager.getConnection(url, user, password);
-//             PreparedStatement statement =
-//                     connection.prepareStatement("SELECT * FROM employee WHERE id=(6)")) {
-//            ResultSet resultSet = statement.executeQuery();
-//                while (resultSet.next()) {
-//                int idOfEmployee = resultSet.getInt("id");
-//                System.out.println("ID сотрудника: " + idOfEmployee);
-//
-//                String nameOfEmployee = resultSet.getString("first_name");
-//                String surenameOfEmployee = resultSet.getString("last_name");
-//                String genderOfEmployee = resultSet.getString("gender");
-//                int ageOfEmployee = resultSet.getInt("age");
-//
-//
-//                System.out.println("Имя, Фамилия: " + nameOfEmployee + " " + surenameOfEmployee);
-//                System.out.println("Пол сотрудника: " + genderOfEmployee);
-//                System.out.println("Возраст сотрудника: " + ageOfEmployee);
-//
-//            }
-//
-//        } catch (SQLException e) {
-//            System.out.println("Ошибка при подключении к базе данных!");
-//            e.printStackTrace();
-//        }
-//
-//        employeeDAO.getAllEmployee();
-//        System.out.println("---------------");
-//        //удалить сотрудника:
-//        employeeDAO.deleteEmployee(7);
-//        employeeDAO.getAllEmployee();
-//        System.out.println("---------------");
-//        employeeDAO.updateEmployee(1,employee1);
-//        employeeDAO.getAllEmployee();
-//        System.out.println("---------------");
-//        employeeDAO.getById(5);
     }
-
 }
